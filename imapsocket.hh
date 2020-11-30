181de96e84b98b2c2be66fa49b86f0bb86b3421c
RamilAkhmetov <ramil.akhmetov.2004@gmail.com>
2020-11-30 23:14:11 +0500

    Begin to copy code that I don't understand well :)

--- imapsocket.hh
+++ imapsocket.hh
@@ -1,4 +1,30 @@
 #ifndef IMAPSOCKET_HH
 #define IMAPSOCKET_HH
 
+#include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+
+class MyTcpSocket : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpSocket(QObject *parent = 0);
+
+    void doConnect();
+
+signals:
+
+public slots:
+    void connected();
+    void disconnected();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+
+private:
+    QTcpSocket *socket;
+
+};
+
 #endif // IMAPSOCKET_HH
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.hh"
-
+#include "imapsocket.hh"
 #include <QApplication>
 
 int main(int argc, char *argv[])
